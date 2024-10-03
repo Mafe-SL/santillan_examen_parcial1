6d0f7040fe103d8c47c84e8f337135a021d79ad2
Mafe-SL <fernanda.mariastb@gmail.com>
2024-10-03 14:32:50 -0600

    cambios para el examen de  Fernanda Santillan

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -28,7 +28,7 @@ function IndexHeader() {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/img/header.jpg") + ")"
+            backgroundImage: "url(" + require("assets/img/header.jpg") + ")",
           }}
           ref={pageHeader}
         ></div>
@@ -37,10 +37,12 @@ function IndexHeader() {
             <img
               alt="..."
               className="n-logo"
-              src={require("assets/img/now-logo.png")}
+              src={require("assets/img/a.jpg")}
             ></img>
-            <h1 className="h1-seo">Now UI Kit.</h1>
-            <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
+            <h1 className="h1-seo">UNIVERSIDAD TECNOLÓGICA DE DURANGO.</h1>
+            <h2>NOMBRE DEL ALUMNO: SANTILLÁN LEDESMA MARÍA FERNANDA</h2>
+            <h3>GESTIÓN DEL PROCESO DE SOFTWARE</h3>
+            <h4>PARCIAL I</h4>
           </div>
           <h6 className="category category-absolute">
             Designed by{" "}
